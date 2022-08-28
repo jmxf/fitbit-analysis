8e1315ea4f082f577686a12ed04149e305333c9b
jmxf <j.moxam-fangmann@outlook.com>
2022-08-28 16:25:05 +0100

    remove redundant test cases

--- data-loading-tests.R
+++ data-loading-tests.R
@@ -12,18 +12,3 @@ json_data2 <- RJSONIO::fromJSON(
   simplify = TRUE
 )
 
-
-parser <- newJSONParser()
-parser$addData( json_data )
-food <- parser$getObject()
-print( food )
-
-
-test_df <- data.frame(
-  "Dates" = c("2022-04-01", "2022-03-31", "2022-03-30", "2022-03-29"),
-  "Weekday" = c("Freitag", "Donnerstag", "Mittwoch", "Dienstag")
-)
-
-weekday_json <- toJSON(test_df)
-print(weekday_json)
-write(weekday_json, file = "test_file.json")
